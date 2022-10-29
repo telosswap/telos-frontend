3a9cd974a9b401135501d3b330f58d54bac47a5a
Altbee <liaojikundever@gmail.com>
2022-10-28 21:52:28 -0500

    hide cakee

--- src/packages/uikit/src/components/Footer/Footer.tsx
+++ src/packages/uikit/src/components/Footer/Footer.tsx
@@ -33,7 +33,7 @@ const MenuItem: React.FC<FooterProps> = ({
   return (
     <StyledFooter p={['40px 16px', null, '56px 40px 32px 40px']} {...props} justifyContent="center">
       <Flex flexDirection="column" width={['100%', null, '1200px;']}>
-        <StyledIconMobileContainer display={['block', null, 'none']}>
+        <StyledIconMobileContainer display={['none', null, 'none']}>
           <LogoWithTextIcon isDark width="130px" />
         </StyledIconMobileContainer>
         <Flex
