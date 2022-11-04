7ad792b31eaaee08a37237bd002f7f710272946e
Altbee <liaojikundever@gmail.com>
2022-11-03 20:38:09 -0500

    update chart bg

--- src/views/Swap/components/Chart/styles.tsx
+++ src/views/Swap/components/Chart/styles.tsx
@@ -11,9 +11,9 @@ export const StyledPriceChart = styled(Box)<{
   width: 100%;
   padding-top: 36px;
   z-index: 2;
+  background: white;
   ${({ theme }) => theme.mediaQueries.sm} {
     padding-top: 8px;
-    background: ${({ $isDark }) => ($isDark ? 'rgba(39, 38, 44, 0.5)' : 'rgba(255, 255, 255, 0.5)')};
     border: ${({ theme }) => `1px solid ${theme.colors.cardBorder}`};
     border-radius: ${({ $isExpanded }) => ($isExpanded ? '0' : '16px')};
     width: ${({ $isExpanded, $isFullWidthContainer }) => ($isFullWidthContainer || $isExpanded ? '100%' : '50%')};
